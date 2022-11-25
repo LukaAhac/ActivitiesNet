93f2ed18d1d92175a729b48d1478a5d3971a070d
Luka <51526367+LukaAhac@users.noreply.github.com>
2022-11-25 22:30:28 +0100

    adding central agent to manage api calls

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,10 @@
 import React, { Fragment, useEffect, useState } from 'react';
-import axios from 'axios';
 import {Container} from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import {v4 as uuid} from 'uuid';
+import agent from './api/agent';
 
 function App() {
 const [activities, setActivities] = useState<Activity[]>([]);
@@ -12,8 +12,8 @@ const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(u
 const [editMode, setEditMode] = useState(false);
 
 useEffect(() => {
-  axios.get<Activity[]>("http://localhost:5000/api/activities").then(response => {
-    setActivities(response.data);
+  agent.Activites.list().then(response => {
+    setActivities(response);
   })
 }, [])
 
