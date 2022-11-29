6c029346ac37e21ecd3c2b62e5b87d4a844a460e
Luka <51526367+LukaAhac@users.noreply.github.com>
2022-11-29 15:27:12 +0100

    redoing style for activity items

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -4,26 +4,23 @@ import { Header, Item, Segment } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 import ActivityListItem from './ActivityListItem';
 
-export default observer(function ActivityList(){
-    const {activityStore} = useStore();
-    const {groupedActivities} = activityStore;
+export default observer(function ActivityList() {
+    const { activityStore } = useStore();
+    const { groupedActivities } = activityStore;
 
 
 
-    return(
+    return (
         <>
             {groupedActivities.map(([group, activities]) => (
                 <Fragment key={group}>
                     <Header sub color='teal'>
                         {group}
                     </Header>
-                    <Segment>
-            <Item.Group divided>
-                {activities.map(activity => (
-                    <ActivityListItem key={activity.id} activity={activity} />
-                ))}
-            </Item.Group>
-        </Segment>
+
+                    {activities.map(activity => (
+                        <ActivityListItem key={activity.id} activity={activity} />
+                    ))}
                 </Fragment>
             ))}
         </>
