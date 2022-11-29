2d06aa1fb33840ef891879bc26186a326af7309a
Luka <51526367+LukaAhac@users.noreply.github.com>
2022-11-29 14:39:16 +0100

    adding home page

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -13,16 +13,22 @@ function App() {
 
   return (
     <>
-      <NavBar />
-      <Container style={{ marginTop: '7em' }}>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/activities' element={<ActivityDashboard />} />
-          <Route path='/activities/:id' element={<ActivityDetails />} />
-          <Route key={location.key} path='/createActivity' element={<ActivityForm />} />
-          <Route key={location.key} path='/manage/:id' element={<ActivityForm />} />
-        </Routes>
-      </Container>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path={'/*'} element={
+          <>
+            <NavBar />
+            <Container style={{ marginTop: '7em' }}>
+              <Routes>
+                <Route path='/activities' element={<ActivityDashboard />} />
+                <Route path='/activities/:id' element={<ActivityDetails />} />
+                <Route key={location.key} path='/createActivity' element={<ActivityForm />} />
+                <Route key={location.key} path='/manage/:id' element={<ActivityForm />} />
+              </Routes>
+            </Container>
+          </>
+        } />
+      </Routes>
     </>
   );
 }
