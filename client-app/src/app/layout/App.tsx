e0112a238d44428d2ce5d4b7feeee488c86e8dda
Luka <51526367+LukaAhac@users.noreply.github.com>
2022-11-30 01:19:28 +0100

    adding not found comp and redirect err 404 to it

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -9,6 +9,7 @@ import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
 import TestErrors from '../../features/errors/TestError';
 import { ToastContainer } from 'react-toastify';
+import NotFound from '../../features/errors/NotFound';
 
 function App() {
   const location = useLocation();
@@ -25,9 +26,10 @@ function App() {
               <Routes>
                 <Route path='/activities' element={<ActivityDashboard />} />
                 <Route path='/activities/:id' element={<ActivityDetails />} />
-                <Route key={location.key} path='/createActivity' element={<ActivityForm />} />
-                <Route key={location.key} path='/manage/:id' element={<ActivityForm />} />
+                <Route path='/createActivity' key={location.key} element={<ActivityForm />} />
+                <Route path='/manage/:id' key={location.key} element={<ActivityForm />} />
                 <Route path='/errors' element={<TestErrors/>} />
+                <Route path='*' element={<NotFound/>}/>
               </Routes>
             </Container>
           </>
